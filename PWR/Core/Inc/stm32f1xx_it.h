982ff1bb31a1bbde1b9f52a60a8d3b9fe352f1ae
jakub-zaton <kuba.zaton@interia.pl>
2023-04-09 18:58:32 +0200

    not tested for dłuższy czas

--- PWR/Core/Inc/stm32f1xx_it.h
+++ PWR/Core/Inc/stm32f1xx_it.h
@@ -56,6 +56,7 @@ void SysTick_Handler(void);
 void DMA1_Channel1_IRQHandler(void);
 void DMA1_Channel4_IRQHandler(void);
 void CAN1_RX1_IRQHandler(void);
+void I2C2_EV_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
